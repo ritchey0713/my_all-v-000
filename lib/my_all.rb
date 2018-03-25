ec82221b6595b87e748a30f6cbe523be899599a7
David Ritchey <ritchey0713@gmail.com>
2018-03-25 01:28:15 +0000

    automatically backed up by learn

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -7,7 +7,12 @@ def my_all?(collection)
  block_return_values << yield(collection[i])
  i = i + 1 
  end 
-block_return_values
+ 
+if block_return_values.include?(false)
+  false
+else 
+  true 
+end
 end
 
 my_all([1,2,3]) {|num| num i < 2}
